9c91e452a867a49d50564256a1878942959ed701
Sam Shearer <sshearer101@gmail.com>
2021-09-01 18:14:09 -0600

    commit tree 875c3d4d5791963593b9037febe90fa196fecb4c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length, :sides, :size
+
+  def initialize(length, sides, size)
+    @length = length
+    @sides = sides
+    @size = size
+  end
+
+  def kind()
+  end
+
+  class TriangleError < StandardError
+    "SOS"
+  end
+
 end
